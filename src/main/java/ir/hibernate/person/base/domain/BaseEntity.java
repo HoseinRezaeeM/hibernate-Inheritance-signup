6cfd063c42ed83e4055aba062d8e3cbe8e9104e3
HoseinRezaei <rezaeen77@gmail.com>
2023-11-14 16:00:39 +0330

    edit @Inheritance to Joined

--- src/main/java/ir/hibernate/person/base/domain/BaseEntity.java
+++ src/main/java/ir/hibernate/person/base/domain/BaseEntity.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.io.Serializable;
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 @Getter
 @Setter
 public class BaseEntity<ID extends Serializable> {
