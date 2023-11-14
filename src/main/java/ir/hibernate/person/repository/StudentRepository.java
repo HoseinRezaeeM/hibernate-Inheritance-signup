4ee3459a44ea7ea05e7cbdcf4040efbab3f5ebed
HoseinRezaei <rezaeen77@gmail.com>
2023-11-14 13:03:28 +0330

    create StudentRepository class

---
+++ src/main/java/ir/hibernate/person/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package ir.hibernate.person.repository;
+
+import ir.hibernate.person.base.repository.BaseEntityRepository;
+import ir.hibernate.person.domain.Student;
+
+public interface StudentRepository extends BaseEntityRepository<Student,Integer> {
+}
