a52f3b9674b8f3c823ff99cd1ac0da8dfb46154d
HoseinRezaei <rezaeen77@gmail.com>
2023-11-14 16:12:47 +0330

    Create Application for main

---
+++ src/main/java/ir/hibernate/person/Application.java
@@ -0,0 +1,56 @@
+package ir.hibernate.person;
+
+import ir.hibernate.person.domain.*;
+import ir.hibernate.person.util.ApplicationContex;
+
+public class Application {
+    public static void main(String[] args) {
+        Person person =new Person("Hosein","Rezaei","1377/02/07");
+        Person person1 =new Person("Ali","Ebrahimi","1380/05/10");
+        Person person2 =new Person("Mohsen","Razavi","1368/08/03");
+        Person person3 =new Person("Hassan","Bahrami","1370/01/09");
+        Student student =new Student(113845,"Mathematic");
+        Student student1 =new Student(115421,"Physics");
+        Student student2 =new Student(158237,"Electronic");
+        Student student3 =new Student(198756,"chemistry");
+        Teacher teacher = new Teacher("majid","vaezzade","1400",150, Degree.DOCTORATE, MasterDegree.PROFESSOR,12500000L);
+        Teacher teacher1 = new Teacher("mehdi","vaezzade","1401",160, Degree.MASTER, MasterDegree.PROFESSOR,12700000L);
+        Teacher teacher2 = new Teacher("mohsen","vaezzade","1403",170, Degree.BACHELOR, MasterDegree.ASSOCIATEPROFESSOR,102500000L);
+        Teacher teacher3 = new Teacher("vahid","vaezzade","1399",180, Degree.MASTER, MasterDegree.ASSISTANTPROFESSOR,11500000L);
+       Teacher teacher4 = new Teacher("vahid","vaezzade","1399",200, Degree.BACHELOR, MasterDegree.ASSISTANTPROFESSOR,18500000L);
+
+       ApplicationContex.getPersonServiceImpl().save(person);
+        ApplicationContex.getPersonServiceImpl().save(person1);
+       ApplicationContex.getPersonServiceImpl().save(person2);
+       ApplicationContex.getPersonServiceImpl().save(person3);
+       ApplicationContex.getPersonServiceImpl().save(student);
+       ApplicationContex.getPersonServiceImpl().save(student1);
+        ApplicationContex.getPersonServiceImpl().save(student2);
+        ApplicationContex.getPersonServiceImpl().save(student3);
+       ApplicationContex.getPersonServiceImpl().save(teacher);
+        ApplicationContex.getPersonServiceImpl().save(teacher1);
+        ApplicationContex.getPersonServiceImpl().save(teacher2);
+
+
+        ApplicationContex.getPersonServiceImpl().deleteById(1);
+        ApplicationContex.getPersonServiceImpl().deleteById(7);
+
+        System.out.println(ApplicationContex.getPersonServiceImpl().contains(3));
+        System.out.println(ApplicationContex.getStudentServiceImpl().contains(2));
+
+        ApplicationContex.getStudentServiceImpl().findAll().forEach(System.out::println);
+
+        ApplicationContex.getPersonServiceImpl().update("Mohamad","Razavian",4);
+        ApplicationContex.getPersonServiceImpl().update("Mohamad","Razavian",8);
+        ApplicationContex.getPersonServiceImpl().update("Mohamad","Razavian",10);
+
+        System.out.println(ApplicationContex.getPersonServiceImpl().signUp("Ali", "jamshidi"));
+        System.out.println(ApplicationContex.getStudentServiceImpl().signUp(210, "Mechanic"));
+        System.out.println(ApplicationContex.getTeacherServiceImpl().signUp(500, Degree.MASTER));
+
+        System.out.println(ApplicationContex.getPersonServiceImpl().findById(1));
+        System.out.println(ApplicationContex.getStudentServiceImpl().findById(8));
+        System.out.println(ApplicationContex.getTeacherServiceImpl().findById(10));
+
+    }
+}
