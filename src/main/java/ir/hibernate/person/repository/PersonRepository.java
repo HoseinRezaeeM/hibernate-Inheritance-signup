0a30e449c1fb8aecdedb4bd038d61aa617382c3d
HoseinRezaei <rezaeen77@gmail.com>
2023-11-14 12:47:08 +0330

    create PersonRepository class

---
+++ src/main/java/ir/hibernate/person/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package ir.hibernate.person.repository;
+
+import ir.hibernate.person.base.repository.BaseEntityRepository;
+import ir.hibernate.person.domain.Person;
+
+public interface PersonRepository extends BaseEntityRepository<Person,Integer> {
+}
