9237c0ff167bc219508e12b416c2493ccce42c15
HoseinRezaei <rezaeen77@gmail.com>
2023-11-14 12:47:16 +0330

    create PersonRepositoryImpl class

---
+++ src/main/java/ir/hibernate/person/repository/impl/PersonRepositoryImpl.java
@@ -0,0 +1,18 @@
+package ir.hibernate.person.repository.impl;
+
+import ir.hibernate.person.base.repository.impl.BaseEntityRepositoryImpl;
+import ir.hibernate.person.domain.Person;
+import ir.hibernate.person.repository.PersonRepository;
+import org.hibernate.Session;
+
+public class PersonRepositoryImpl extends BaseEntityRepositoryImpl<Person, Integer>
+        implements PersonRepository {
+    public PersonRepositoryImpl(Session session) {
+        super(session);
+    }
+
+    @Override
+    protected String getEntityTableName() {
+        return "Person";
+    }
+}
