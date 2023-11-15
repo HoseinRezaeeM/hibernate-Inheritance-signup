b526c6ea829d8db60be4a6355a1425180b12866e
HoseinRezaei <rezaeen77@gmail.com>
2023-11-15 13:16:56 +0330

    edit ValidationStudent class and add method

---
+++ src/main/java/ir/hibernate/person/validation/ValidationStudent.java
@@ -0,0 +1,41 @@
+package ir.hibernate.person.validation;
+
+import ir.hibernate.person.domain.Student;
+import ir.hibernate.person.util.ApplicationContex;
+import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
+import org.xml.sax.SAXException;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.io.IOException;
+import java.util.Set;
+
+public class ValidationStudent {
+
+    public static void validationforSaveStudent(Student student) throws IOException, SAXException {
+        ValidatorFactory factory;
+        factory=Validation.byDefaultProvider().configure()
+                .messageInterpolator(new ParameterMessageInterpolator())
+                .buildValidatorFactory();
+        Validator validator;
+        validator=factory.usingContext()
+                .messageInterpolator(new ParameterMessageInterpolator())
+                .getValidator();
+
+        Set<ConstraintViolation<Student>> violations = validator.validate(student);
+
+        if (violations.isEmpty()) {
+            ApplicationContex.getStudentServiceImpl().save(student);
+            System.out.println("Teacher saved successfully.");
+        } else {
+            for (ConstraintViolation<Student> violation : violations) {
+                System.out.println(violation.getPropertyPath() + ": " + violation.getMessage());
+            }
+            System.out.println("Teacher validation failed. Not saving to the database.");
+        }
+
+        factory.close();
+    }
+}
