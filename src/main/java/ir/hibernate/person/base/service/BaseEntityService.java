88b810afe2b26888ab6274372609bf14066cb29d
HoseinRezaei <rezaeen77@gmail.com>
2023-11-15 13:18:14 +0330

    change BaseEntityService

--- src/main/java/ir/hibernate/person/base/service/BaseEntityService.java
+++ src/main/java/ir/hibernate/person/base/service/BaseEntityService.java
@@ -1,13 +1,15 @@
 package ir.hibernate.person.base.service;
 
 import ir.hibernate.person.base.domain.BaseEntity;
+import org.xml.sax.SAXException;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
 public interface BaseEntityService<T extends BaseEntity<ID>, ID extends Serializable> {
 
-    void save(T entity);
+    void save(T entity) throws IOException, SAXException;
 
     void update(String firstname, String lastname, ID id);
 
